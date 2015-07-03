fcd7c7190e7d7cce27f9d424174cfddec235013b
Jack Parsons <jack.parsons1995@hotmail.com>
2015-07-03 13:11:23 +0100

    Progress on fetching specific name

--- fetch.py
+++ fetch.py
@@ -1,23 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 class buildDB(object):
 	#check version colour and author(orphan) in future, thread for data fetching + another for writing?
-	def __init__(self):
-		site = 'https://aur.archlinux.org/packages/?O=0&K=&PP=250'
-		r = requests.get(site, stream=True)
-		soup = BeautifulSoup(r.content)
-		print(soup.prettify)
+    def __init__(self):
+        self.site = 'https://aur.archlinux.org/packages/?O=0&K=&PP=250'
+        self.r = requests.get(self.site, stream=True)
+        self.soup = BeautifulSoup(self.r.content)
+        print(self.soup.prettify)
 
-	def get_list(self):
-		temp_array = self.soup.find_all('p',limit=2)[1].string
-		a = str(temp_array[1])
-		b = a.split(' ')
-		limit = b[len(b)-1].split('.')[0]
-		for i in range(0,limit+1):
-			site = 'https://aur.archlinux.org/packages/?O='+str(250*i)+'&K=&PP=250'
-			r = requests.get(site, stream=True)
-			soup = BeautifulSoup(r.content)
-			rows = soup.find_all('tr')
-			for row in rows:
-				print(soup.find_all('a'))
+    def get_list(self):
+        temp_array = self.soup.find_all('p',limit=2)[1].string
+        a = re.sub(r"[\n]?[\t]+","",temp_array).split('.')
+        b = a[1].split(' ')
+        limit = int(b[len(b)-1])
+        for i in range(0,limit+1):
+            self.site = 'https://aur.archlinux.org/packages/?O='+str(250*i)+'&K=&PP=250'
+            self.r = requests.get(self.site, stream=True)
+            self.soup = BeautifulSoup(self.r.content)
+            rows = self.soup.html.body.find_all('tr')
+            for row in rows:
+                row = row.find_all('a',limit=1)
+                d = re.sub(r">(.*)<\/a>","",str(row))
+                print(row)
+                print(d)
+                print('{}'.format(i*250))
