33e8931daf6dfb922b42f362f71f0dfea4b222ad
Jack Parsons <jack.parsons1995@hotmail.com>
2015-07-06 22:47:10 +0100

    tab completion work

--- fetch.py
+++ fetch.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
+import sqlite3
 
 class buildDB(object):
 	#check version colour and author(orphan) in future, thread for data fetching + another for writing?
@@ -8,21 +10,25 @@ class buildDB(object):
         self.site = 'https://aur.archlinux.org/packages/?O=0&K=&PP=250'
         self.r = requests.get(self.site, stream=True)
         self.soup = BeautifulSoup(self.r.content)
-        print(self.soup.prettify)
+       # if not os.path.isdir(os.path.expanduser('~')+".aurdb"):
+       #     os.makedirs(os.path.expanduser('~')+"/.aurdb")
+        if not os.path.exists(os.path.expanduser('~')+".packages.db"):
+            conn = sqlite3.connect('packages.db')
+            self.c = conn.cursor()
 
+            
     def get_list(self):
-        temp_array = self.soup.find_all('p',limit=2)[1].string
-        a = re.sub(r"[\n]?[\t]+","",temp_array).split('.')
+        temp_array = self.soup.select("#pkglist-results > div > p:nth-of-type(1)")
+        a = re.sub(r"[\n]?[\t]+","",temp_array[0].string).split('.')
         b = a[1].split(' ')
         limit = int(b[len(b)-1])
         for i in range(0,limit+1):
             self.site = 'https://aur.archlinux.org/packages/?O='+str(250*i)+'&K=&PP=250'
             self.r = requests.get(self.site, stream=True)
             self.soup = BeautifulSoup(self.r.content)
-            rows = self.soup.html.body.find_all('tr')
-            for row in rows:
-                row = row.find_all('a',limit=1)
-                d = re.sub(r">(.*)<\/a>","",str(row))
-                print(row)
-                print(d)
-                print('{}'.format(i*250))
+            names = self.soup.select(".results > tbody:nth-of-type(1) > tr > td:nth-of-type(2) > a")
+            for name in names:
+                #Write it to file
+        #        out = open(os.path.expanduser('~') + "/.aurdb/" + name.string,'w')
+        #        out.close()
+                
