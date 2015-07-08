d3d8b46e9f59db2ac5e5292b46ab3a549cd76242
Jack Parsons <jack.parsons1995@hotmail.com>
2015-07-08 12:03:42 +0100

    It actually sort of works now!

--- fetch.py
+++ fetch.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import os
-import sqlite3
+import string
 
 class buildDB(object):
 	#check version colour and author(orphan) in future, thread for data fetching + another for writing?
@@ -12,11 +12,12 @@ class buildDB(object):
         self.soup = BeautifulSoup(self.r.content)
        # if not os.path.isdir(os.path.expanduser('~')+".aurdb"):
        #     os.makedirs(os.path.expanduser('~')+"/.aurdb")
-        if not os.path.exists(os.path.expanduser('~')+".packages.db"):
-            conn = sqlite3.connect('packages.db')
-            self.c = conn.cursor()
+        if not (os.path.isdir(os.path.expanduser('~')+"/.packages")):
+            os.makedirs(os.path.expanduser('~')+"/.packages")
+            for char in string.ascii_lowercase+string.digits:
+                os.makedirs(os.path.expanduser('~')+"/.packages/"+char)
+        #else:
 
-            
     def get_list(self):
         temp_array = self.soup.select("#pkglist-results > div > p:nth-of-type(1)")
         a = re.sub(r"[\n]?[\t]+","",temp_array[0].string).split('.')
@@ -28,7 +29,8 @@ class buildDB(object):
             self.soup = BeautifulSoup(self.r.content)
             names = self.soup.select(".results > tbody:nth-of-type(1) > tr > td:nth-of-type(2) > a")
             for name in names:
+                fileObj = open((os.path.expanduser('~')) + "/.packages/"+name.string[0:1:]+"/"+name.string,'w')
+                fileObj.close()
                 #Write it to file
         #        out = open(os.path.expanduser('~') + "/.aurdb/" + name.string,'w')
         #        out.close()
-                
---
+++ readme.md
@@ -0,0 +1,2 @@
+TODO:
+Change how it works, simply download list from aur, get all packages in sync repos dir, merge to list, check performance
